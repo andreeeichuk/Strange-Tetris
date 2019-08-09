66d0b6e5a5541a90bfd91f92d34158655e63af38
Ivan Andreichuk <andreeeichuk@gmail.com>
2019-08-09 12:40:59 +0300

    Add Context

---
+++ Assets/Scripts/TetrisContext.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using strange.extensions.context.api;
+using strange.extensions.context.impl;
+using strange.extensions.dispatcher.eventdispatcher.api;
+using strange.extensions.dispatcher.eventdispatcher.impl;
+using strange.extensions.command.api;
+using strange.extensions.command.impl;
+
+public class TetrisContext : MVCSContext
+{
+    public TetrisContext(MonoBehaviour view) : base(view)
+    {
+    }
+
+    public TetrisContext(MonoBehaviour view, ContextStartupFlags flags) : base(view, flags)
+    {
+    }
+
+    protected override void addCoreComponents()
+    {
+        base.addCoreComponents();
+        injectionBinder.Unbind<ICommandBinder>();
+        injectionBinder.Bind<ICommandBinder>().To<SignalCommandBinder>().ToSingleton();
+    }
+
+    override public IContext Start()
+    {
+        base.Start();
+        /// insert start signal here
+        return this;
+    }
+
+    protected override void mapBindings()
+    {
+        
+    }
+}
