43e6c7f278c8fb7fe0d3602b39601c55968c894d
Ivan Andreichuk <andreeeichuk@gmail.com>
2019-08-09 13:27:47 +0300

    Add Grid model interface and implementation

---
+++ Assets/Scripts/Models/GridModel.cs
@@ -0,0 +1,19 @@
+﻿public class GridModel : IGridModel
+{
+    private bool[,] cells;
+    
+    public void Create(int height, int width)
+    {
+        cells = new bool[height, width];
+    }
+
+    public bool IsCellFilled(int x, int y)
+    {
+        return cells[x, y];
+    }
+
+    public void SetCell(int x, int y, bool isFilled)
+    {
+        cells[x, y] = isFilled;
+    }
+}
---
+++ Assets/Scripts/Models/Interfaces/IGridModel.cs
@@ -0,0 +1,6 @@
+﻿public interface IGridModel
+{
+    void Create(int x, int y);
+    bool IsCellFilled(int x, int y);
+    void SetCell(int x, int y, bool isFilled);
+}
