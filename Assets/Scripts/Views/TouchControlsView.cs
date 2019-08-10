e445aecf434d8d28b2955a94220c814c35de7861
Ivan Andreichuk <andreeeichuk@gmail.com>
2019-08-10 20:51:21 +0300

    Fix multitouch

--- Assets/Scripts/Views/TouchControlsView.cs
+++ Assets/Scripts/Views/TouchControlsView.cs
@@ -8,23 +8,39 @@ public class TouchControlsView : ControlsView
     {
         if (Input.touches.Length>0)
         {
-            foreach(var touch in Input.touches)
+            //foreach(var touch in Input.touches)
+            //{
+            //    if(touch.phase == TouchPhase.Began)
+            //    {
+            //        TouchBegin.Dispatch(Camera.main.ScreenToWorldPoint(touch.position));
+            //    }
+
+            //    if(touch.phase == TouchPhase.Moved)
+            //    {
+            //        TouchContinue.Dispatch(Camera.main.ScreenToWorldPoint(touch.position));
+            //    }
+
+            //    if (touch.phase == TouchPhase.Ended)
+            //    {
+            //        TouchEnd.Dispatch(Camera.main.ScreenToWorldPoint(touch.position));
+            //    }
+            //}
+            Touch firstTouch = Input.touches[0];
+
+            if (firstTouch.phase == TouchPhase.Began)
             {
-                if(touch.phase == TouchPhase.Began)
-                {
-                    TouchBegin.Dispatch(Camera.main.ScreenToWorldPoint(touch.position));
-                }
-
-                if(touch.phase == TouchPhase.Moved)
-                {
-                    TouchContinue.Dispatch(Camera.main.ScreenToWorldPoint(touch.position));
-                }
-
-                if (touch.phase == TouchPhase.Ended)
-                {
-                    TouchEnd.Dispatch(Camera.main.ScreenToWorldPoint(touch.position));
-                }
+                TouchBegin.Dispatch(Camera.main.ScreenToWorldPoint(firstTouch.position));
             }
-        }        
+
+            if (firstTouch.phase == TouchPhase.Moved)
+            {
+                TouchContinue.Dispatch(Camera.main.ScreenToWorldPoint(firstTouch.position));
+            }
+
+            if (firstTouch.phase == TouchPhase.Ended || firstTouch.phase == TouchPhase.Canceled)
+            {
+                TouchEnd.Dispatch(Camera.main.ScreenToWorldPoint(firstTouch.position));
+            }
+        }
     }
 }
